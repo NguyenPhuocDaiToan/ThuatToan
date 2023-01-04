637a486803cc7090eda1645814e9be0b7eeadbb0
DAITOAN <daitoan2000@gmail.com>
2023-01-04 10:41:56 +0700

    Toan.NPD submit bai27

---
+++ bai27.js
@@ -0,0 +1,17 @@
+// Bài 27: Đếm chữ số lớn nhất của số nguyên dương n bằng Javascript
+
+let n = 97966386;
+let max = 0;
+let count = 1;
+do {
+    if(n % 10 === max) {
+        count++;
+    } else if(n % 10 > max) {
+        max = n % 10;
+        count = 1;
+    } 
+    n = Number.parseInt(n / 10);
+} while(n !== 0);
+
+console.log(count);
+// Output: 2
