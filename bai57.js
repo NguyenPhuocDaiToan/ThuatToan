3dd5bf426b1ce47b5d944a94e697d66b45eeb909
DAITOAN <daitoan2000@gmail.com>
2023-01-04 16:50:05 +0700

    Toan.NPD submit bai57

---
+++ bai57.js
@@ -0,0 +1,8 @@
+// Bài 57: Liệt kê vị trí các giá trị âm trong mảng một chiều các số thực
+
+let a = [1, -2, 3, 4, -5, 6, 7, 8, 9];
+a.forEach(t => {
+    if(t < 0) {
+        console.log(t);
+    }
+})
